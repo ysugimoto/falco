c19e58b6c4e31033cc834b48887bdcaff110dcf7
ysugimoto <sugimoto@wnotes.net>
2022-04-08 20:00:24 +0900

    fix typo

--- linter/linter_test.go
+++ linter/linter_test.go
@@ -355,7 +355,7 @@ sub baz {
 	set var.item1 = "1";
 	set var.item2 = 1;
 	set var.item3 = 1.0;
-	set var.item4 = std.ip("192.168.0.1", "192.168.0,2");
+	set var.item4 = std.ip("192.168.0.1", "192.168.0.2");
 	set var.item5 = always;
 	set var.item6 = foo;
 	set var.item7 = bar;
