64a65a515d1601ec44810628a43f06ae07bc61b9
ysugimoto <sugimoto@wnotes.net>
2023-03-14 01:28:11 +0900

    added director testings

--- interpreter/director_test.go
+++ interpreter/director_test.go
@@ -44,6 +44,12 @@ func createTestInterpreter(director string) (*Interpreter, error) {
 	}
 	ip := New()
 	ip.ctx = context.New()
+	ip.ctx.RequestHash = &value.String{
+		Value: "/?foo=bar",
+	}
+	ip.ctx.ClientIdentity = &value.String{
+		Value: "127.0.0.1",
+	}
 	if err := ip.ProcessStatements(vcl.Statements); err != nil {
 		return nil, fmt.Errorf("Failed to process statement: %s", err)
 	}
@@ -135,13 +141,13 @@ director test random {
 		b02 := results[ip.ctx.Backends["test02"]] / 100
 		b03 := results[ip.ctx.Backends["test03"]] / 100
 		if b01 < 48 || b01 > 52 {
-			t.Errorf("test01 backend determined around 50%% probablity, got %d%%", b01)
+			t.Errorf("test01 backend determined around 50%% probability, got %d%%", b01)
 		}
 		if b02 < 23 || b02 > 27 {
-			t.Errorf("test02 backend determined around 25%% probablity, got %d%%", b02)
+			t.Errorf("test02 backend determined around 25%% probability, got %d%%", b02)
 		}
 		if b03 < 23 || b03 > 27 {
-			t.Errorf("test03 backend determined around 25%% probablity, got %d%%", b03)
+			t.Errorf("test03 backend determined around 25%% probability, got %d%%", b03)
 		}
 	})
 
@@ -171,10 +177,10 @@ director test random {
 		b01 := results[ip.ctx.Backends["test01"]] / 100
 		b02 := results[ip.ctx.Backends["test02"]] / 100
 		if b01 < 65 || b01 > 68 {
-			t.Errorf("test01 backend determined around 66%% probablity, got %d%%", b01)
+			t.Errorf("test01 backend determined around 66%% probability, got %d%%", b01)
 		}
 		if b02 < 31 || b02 > 34 {
-			t.Errorf("test02 backend determined around 33%% probablity, got %d%%", b02)
+			t.Errorf("test02 backend determined around 33%% probability, got %d%%", b02)
 		}
 		// unhealthy backend should not be determined
 		if _, ok := results[ip.ctx.Backends["test03"]]; ok {
@@ -228,10 +234,233 @@ director test fallback {
 }
 
 func TestContentDirector(t *testing.T) {
+	director := `
+director test hash {
+  .quorum  = 50%;
+  { .backend = test01; .weight = 1; }
+  { .backend = test02; .weight = 1; } { .backend = test03; .weight = 1; }
+}
+`
+	t.Run("Expect quorum weight not reached error", func(t *testing.T) {
+		ip, err := createTestInterpreter(director)
+		if err != nil {
+			t.Errorf("Failed to create interprete: %s", err)
+		}
+		d := ip.ctx.Backends["test"].Director
+
+		// set unhealthy
+		ip.ctx.Backends["test01"].Healthy.Store(false)
+		ip.ctx.Backends["test02"].Healthy.Store(false)
+
+		_, err = ip.directorBackendHash(d)
+		if err != ErrQuorumWeightNotReached {
+			t.Errorf("Content director should return quorum error: %s", err)
+		}
+	})
+
+	t.Run("Determined same backend", func(t *testing.T) {
+		ip, err := createTestInterpreter(director)
+		if err != nil {
+			t.Errorf("Failed to create interprete: %s", err)
+		}
+		d := ip.ctx.Backends["test"].Director
+		results := map[*value.Backend]int{}
+
+		for i := 0; i < 10000; i++ {
+			r, err := ip.directorBackendHash(d)
+			if err != nil {
+				t.Errorf("Hash director backend determination failed: %s", err)
+				return
+			}
+			if _, ok := results[r]; !ok {
+				results[r] = 0
+			}
+			results[r]++
+		}
+
+		// Hash result always choose same backend
+		b01 := results[ip.ctx.Backends["test01"]] / 100
+		b02 := results[ip.ctx.Backends["test02"]] / 100
+		b03 := results[ip.ctx.Backends["test03"]] / 100
+		if b01 != 0 {
+			t.Errorf("test01 backend determined 0%% probability, got %d%%", b01)
+		}
+		if b02 != 100 {
+			t.Errorf("test02 backend determined 100%% probability, got %d%%", b02)
+		}
+		if b03 != 0 {
+			t.Errorf("test03 backend determined 0%% probability, got %d%%", b03)
+		}
+	})
 }
 
 func TestClientDirector(t *testing.T) {
+	director := `
+director test client {
+  .quorum  = 50%;
+  { .backend = test01; .weight = 1; }
+  { .backend = test02; .weight = 1; }
+  { .backend = test03; .weight = 1; }
+}
+`
+	t.Run("Expect quorum weight not reached error", func(t *testing.T) {
+		ip, err := createTestInterpreter(director)
+		if err != nil {
+			t.Errorf("Failed to create interprete: %s", err)
+		}
+		d := ip.ctx.Backends["test"].Director
+
+		// set unhealthy
+		ip.ctx.Backends["test01"].Healthy.Store(false)
+		ip.ctx.Backends["test02"].Healthy.Store(false)
+
+		_, err = ip.directorBackendClient(d)
+		if err != ErrQuorumWeightNotReached {
+			t.Errorf("Client director should return quorum error: %s", err)
+		}
+	})
+
+	t.Run("Determined same backend", func(t *testing.T) {
+		ip, err := createTestInterpreter(director)
+		if err != nil {
+			t.Errorf("Failed to create interprete: %s", err)
+		}
+		d := ip.ctx.Backends["test"].Director
+		results := map[*value.Backend]int{}
+
+		for i := 0; i < 10000; i++ {
+			r, err := ip.directorBackendClient(d)
+			if err != nil {
+				t.Errorf("Client director backend determination failed: %s", err)
+				return
+			}
+			if _, ok := results[r]; !ok {
+				results[r] = 0
+			}
+			results[r]++
+		}
+
+		// Hash result always choose same backend
+		b01 := results[ip.ctx.Backends["test01"]] / 100
+		b02 := results[ip.ctx.Backends["test02"]] / 100
+		b03 := results[ip.ctx.Backends["test03"]] / 100
+		if b01 != 100 {
+			t.Errorf("test01 backend determined 100%% probability, got %d%%", b01)
+		}
+		if b02 != 0 {
+			t.Errorf("test02 backend determined 0%% probability, got %d%%", b02)
+		}
+		if b03 != 0 {
+			t.Errorf("test03 backend determined 0%% probability, got %d%%", b03)
+		}
+	})
 }
 
 func TestChashDirector(t *testing.T) {
+	director := `
+director test chash {
+  .quorum = 50%;
+  .seed   = 1;
+  .key    = object;
+  { .backend = test01; .id = "b01"; }
+  { .backend = test02; .id = "b02"; }
+  { .backend = test03; .id = "b03"; }
+}
+`
+	director2 := `
+director test chash {
+  .quorum = 50%;
+  .seed   = 1;
+  .key    = client;
+  { .backend = test01; .id = "b01"; }
+  { .backend = test02; .id = "b02"; }
+  { .backend = test03; .id = "b03"; }
+}
+`
+	t.Run("Expect quorum weight not reached error", func(t *testing.T) {
+		ip, err := createTestInterpreter(director)
+		if err != nil {
+			t.Errorf("Failed to create interprete: %s", err)
+		}
+		d := ip.ctx.Backends["test"].Director
+
+		// set unhealthy
+		ip.ctx.Backends["test01"].Healthy.Store(false)
+		ip.ctx.Backends["test02"].Healthy.Store(false)
+
+		_, err = ip.directorBackendConsistentHash(d)
+		if err != ErrQuorumWeightNotReached {
+			t.Errorf("Chash director should return quorum error: %s", err)
+		}
+	})
+
+	t.Run("Determined same backend for object key", func(t *testing.T) {
+		ip, err := createTestInterpreter(director)
+		if err != nil {
+			t.Errorf("Failed to create interprete: %s", err)
+		}
+		d := ip.ctx.Backends["test"].Director
+		results := map[*value.Backend]int{}
+
+		for i := 0; i < 10000; i++ {
+			r, err := ip.directorBackendConsistentHash(d)
+			if err != nil {
+				t.Errorf("Chash director backend determination failed: %s", err)
+				return
+			}
+			if _, ok := results[r]; !ok {
+				results[r] = 0
+			}
+			results[r]++
+		}
+
+		// Hash result always choose same backend
+		b01 := results[ip.ctx.Backends["test01"]] / 100
+		b02 := results[ip.ctx.Backends["test02"]] / 100
+		b03 := results[ip.ctx.Backends["test03"]] / 100
+		if b01 != 0 {
+			t.Errorf("test01 backend determined 0%% probability, got %d%%", b01)
+		}
+		if b02 != 0 {
+			t.Errorf("test02 backend determined 0%% probability, got %d%%", b02)
+		}
+		if b03 != 100 {
+			t.Errorf("test03 backend determined 100%% probability, got %d%%", b03)
+		}
+	})
+
+	t.Run("Determined same backend for client key", func(t *testing.T) {
+		ip, err := createTestInterpreter(director2)
+		if err != nil {
+			t.Errorf("Failed to create interprete: %s", err)
+		}
+		d := ip.ctx.Backends["test"].Director
+		results := map[*value.Backend]int{}
+
+		for i := 0; i < 10000; i++ {
+			r, err := ip.directorBackendConsistentHash(d)
+			if err != nil {
+				t.Errorf("Chash director backend determination failed: %s", err)
+				return
+			}
+			if _, ok := results[r]; !ok {
+				results[r] = 0
+			}
+			results[r]++
+		}
+
+		// Hash result always choose same backend
+		b01 := results[ip.ctx.Backends["test01"]] / 100
+		b02 := results[ip.ctx.Backends["test02"]] / 100
+		b03 := results[ip.ctx.Backends["test03"]] / 100
+		if b01 != 100 {
+			t.Errorf("test01 backend determined 100%% probability, got %d%%", b01)
+		}
+		if b02 != 0 {
+			t.Errorf("test02 backend determined 0%% probability, got %d%%", b02)
+		}
+		if b03 != 0 {
+			t.Errorf("test03 backend determined 0%% probability, got %d%%", b03)
+		}
+	})
 }
