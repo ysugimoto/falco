38c68bb34530735faad4982145b4a45ea078ebda
ysugimoto <sugimoto@wnotes.net>
2023-09-08 17:02:31 +0900

    integrate to one function, documentation

--- tester/function/testing_fixed_time.go
+++ tester/function/testing_fixed_time.go
@@ -16,16 +16,6 @@ func Testing_fixed_time_lookup_Validate(args []value.Value) error {
 	if len(args) != 1 {
 		return errors.ArgumentNotEnough(Testing_fixed_time_lookup_Name, 1, args)
 	}
-	for i := range args {
-		if args[i].Type() != Testing_fixed_time_lookup_ArgumentTypes[i] {
-			return errors.TypeMismatch(
-				Testing_fixed_time_lookup_Name,
-				i+1,
-				Testing_fixed_time_lookup_ArgumentTypes[i],
-				args[i].Type(),
-			)
-		}
-	}
 	return nil
 }
 
@@ -40,11 +30,27 @@ func Testing_fixed_time(
 		return nil, errors.NewTestingError(err.Error())
 	}
 
-	fixed := value.Unwrap[*value.String](args[0]).Value
-	ft, err := time.Parse(expectedTimeFormat, fixed)
-	if err != nil {
-		return value.Null, errors.NewTestingError("Invalid time format: %s", err)
+	switch args[0].Type() {
+	case value.IntegerType:
+		v := value.Unwrap[*value.Integer](args[0])
+		t := time.Unix(v.Value, 0)
+		ctx.FixedTime = &t
+	case value.TimeType:
+		t := value.Unwrap[*value.Time](args[0]).Value
+		ctx.FixedTime = &t
+	case value.StringType:
+		fixed := value.Unwrap[*value.String](args[0]).Value
+		ft, err := time.Parse(expectedTimeFormat, fixed)
+		if err != nil {
+			return value.Null, errors.NewTestingError("Invalid time format: %s", err)
+		}
+		ctx.FixedTime = &ft
+	default:
+		return value.Null, errors.NewTestingError(
+			"First argument of %s must be INTEGER or TIME or STRING type, %s provided",
+			Testing_fixed_time_lookup_Name,
+			args[0].Type(),
+		)
 	}
-	ctx.FixedTime = &ft
 	return value.Null, nil
 }
