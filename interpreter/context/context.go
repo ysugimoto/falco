38d68906569757d4546ceef884bf3414801a8aa5
ysugimoto <sugimoto@wnotes.net>
2026-02-03 12:36:56 +0900

    initialize value

--- interpreter/context/context.go
+++ interpreter/context/context.go
@@ -257,6 +257,7 @@ func New(options ...Option) *Context {
 		BetweenBytesTimeout:                 &value.RTime{},
 		ConnectTimeout:                      &value.RTime{},
 		FirstByteTimeout:                    &value.RTime{Value: 15 * time.Second},
+		FetchTimeout:                        &value.RTime{},
 		BackendResponseGzip:                 &value.Boolean{},
 		BackendResponseBrotli:               &value.Boolean{},
 		BackendResponseCacheable:            &value.Boolean{},
